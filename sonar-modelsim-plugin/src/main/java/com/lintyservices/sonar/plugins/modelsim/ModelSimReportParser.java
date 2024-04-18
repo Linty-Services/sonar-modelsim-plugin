4dc4fd08843f29bfa2bc31b0e1f2b3144cecdc69
David RACODON <david.racodon@gmail.com>
2024-04-18 18:54:47 +0200

    Update license headers with 2024

--- sonar-modelsim-plugin/src/main/java/com/lintyservices/sonar/plugins/modelsim/ModelSimReportParser.java
+++ sonar-modelsim-plugin/src/main/java/com/lintyservices/sonar/plugins/modelsim/ModelSimReportParser.java
@@ -1,6 +1,6 @@
 /*
  * Linty ModelSim :: Plugin
- * Copyright (C) 2019-2023 Linty Services
+ * Copyright (C) 2019-2024 Linty Services
  * mailto:contact@linty-services.com
  *
  * This program is free software; you can redistribute it and/or
